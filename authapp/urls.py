63f1dd18c8b097b0589becb29e23e07297853547
yathish <iamyathi.py@gmail.com>
2023-12-12 12:07:09 +0530

    Adding views, urls and html forms

---
+++ authapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from authapp.views import index, register
+
+app_name = 'authapp'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/', index, name='home'),
+    path('register/', register, name='register')
+]
