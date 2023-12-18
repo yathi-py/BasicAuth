336bf4ae2f6f78129fc19f06515c127404af3a09
yathish <iamyathi.py@gmail.com>
2023-12-18 15:08:58 +0530

    moving home screen

--- BasicAuth/urls.py
+++ BasicAuth/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from authapp.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('authapp.urls')),
+    path('', index, name='home'),
 ]
--- authapp/urls.py
+++ authapp/urls.py
@@ -8,6 +8,5 @@ app_name = 'authapp'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('home/', index, name='home'),
     path('register/', register, name='register')
 ]
